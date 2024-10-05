2fa62f80c7a907171ce80f7be07c8e3488a8adfd
devyanising8799 <sngtgirase@gmail.com>
2024-10-05 11:32:47 +0530

    Add files via upload

---
+++ c2.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int i,j,k;
+	
+           for(i=1; i<=4; i++)
+           {
+               	for(j=1; j<=i; j++)
+    	          {
+    	            	printf("%d",k);
+    		             k++;
+		          }
+		      printf("\n");
+	}
+}
